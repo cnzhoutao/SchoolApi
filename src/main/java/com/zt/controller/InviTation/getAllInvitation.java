5eb715d594aac40f3b91c77e9228c438fbacd897
zt <619598856@qq.com>
2017-09-23 00:36:02 +0800

    浏览量加一

--- src/main/java/com/zt/controller/InviTation/getAllInvitation.java
+++ src/main/java/com/zt/controller/InviTation/getAllInvitation.java
@@ -102,6 +102,7 @@ public class getAllInvitation {
             }
             InviWithDetailImg inviTation = inviTationDaoI.getInviId(inviId);
 
+            inviTationDaoI.viewNumAdd1(inviId);
 
             List<JugeInvi> jugeList=inviJugeDaoI.getJugeByInviId(inviId);
 
